e90164a6da1188dbb832401990466b9decde5a96
wvrodrguesbr <125496585+wvrodrguesbr@users.noreply.github.com>
2023-03-07 11:52:52 -0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vorx-vpc-prod" {
   cidr_block       = "10.0.0.0/16"
 
   tags = {
-    Name = "Vorx-PROD"
+    Name = "Vorx-PRODUCAO"
   }
 }
 
@@ -65,60 +65,3 @@ resource "aws_route_table" "public-rt" {
   tags = {
     Name = "prod-public-rt"
   }
-}
-
-resource "aws_route_table_association" "pub-rt-1a-associate" {
-  subnet_id      = aws_subnet.vorx-subnet-pub-1a.id
-  route_table_id = aws_route_table.public-rt.id
-}
-
-resource "aws_route_table_association" "pub-rt-1b-associate" {
-  subnet_id      = aws_subnet.vorx-subnet-pub-1b.id
-  route_table_id = aws_route_table.public-rt.id
-}
-
-
-resource "aws_route_table" "private-rt-1a" {
-  vpc_id = aws_vpc.vorx-vpc-prod.id
-
-  tags = {
-    Name = "prod-private-rt-1a"
-  }
-}
-
-resource "aws_route_table" "private-rt-1b" {
-  vpc_id = aws_vpc.vorx-vpc-prod.id
-
-  tags = {
-    Name = "prod-private-rt-1b"
-  }
-}
-
-resource "aws_route_table_association" "priv-rt-1a-associate" {
-  subnet_id      = aws_subnet.vorx-subnet-priv-1a.id
-  route_table_id = aws_route_table.private-rt-1a.id
-}
-
-resource "aws_route_table_association" "priv-rt-1b-associate" {
-  subnet_id      = aws_subnet.vorx-subnet-priv-1b.id
-  route_table_id = aws_route_table.private-rt-1b.id
-}
-
-
-
-## OUTPUTS DO NOSSO TF ##
-output "vpc_vorx_prod_id" {
-value = aws_vpc.vorx-vpc-prod.id
-}
-
-output "vpc_vorx_prod_arn" {
-value = aws_vpc.vorx-vpc-prod.arn
-}
-
-output "vorx_prod_subnet_pub-1a" {
-value = aws_subnet.vorx-subnet-pub-1a.id
-}
-
-output "vorx_prod_subnet_priv-1a" {
-value = aws_subnet.vorx-subnet-priv-1a.id
-}
